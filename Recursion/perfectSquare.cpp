1b949c8e6c546c2ca4e92c41abf283341494add5
Saurav Chaudhary <59189555+sauravchaudharysc@users.noreply.github.com>
2020-03-24 23:23:37 +0530

    Add files via upload

---
+++ Recursion/perfectSquare.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int perfectSquare(int n)
+{
+	if(n<=0)
+	{
+		return 0;
+	}
+	else
+	{
+		int min_Value=INT_MAX;
+		int square=1;
+		for(int i=1;square<=n;i++)
+		{
+			min_Value=min(min_Value,perfectSquare(n-square)+1);
+			square=i*i;
+		}
+		return min_Value;
+	}
+}
+int main()
+{
+	cout<<perfectSquare(5);
+}
